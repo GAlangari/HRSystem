b93a03ef081911319cef3e03c1383ba312f5a162
ghaida <ghaidam04@gmail.com>
2023-05-26 16:29:14 +0300

    Add project files.

---
+++ BaseRepository/BaseRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRSystem.BaseRepository
+{
+    class BaseRepository
+    {
+        private readonly string connectionString;
+
+        protected BaseRepository()
+        {
+            connectionString = ConfigurationManager.ConnectionStrings["HRSystem"].ConnectionString;
+        }
+
+        protected SqlConnection GetConnection()
+        {
+            var result = new SqlConnection(connectionString);
+            return result;
+        }
+
+
+    }
+}
